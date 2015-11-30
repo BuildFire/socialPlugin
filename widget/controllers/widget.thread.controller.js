2e77c96dbbf55eee45e7db658798205109fd227f
rishabhdixit <rishabh.dixit@intelligrape.com>
2015-11-30 17:17:31 +0530

    Made username available for each comment on Thread page

--- widget/controllers/widget.thread.controller.js
+++ widget/controllers/widget.thread.controller.js
@@ -4,6 +4,8 @@
     angular.module('socialPluginWidget')
         .controller('ThreadCtrl', ['$scope', '$routeParams', 'SocialDataStore', function ($scope, $routeParams, SocialDataStore) {
             var Thread = this;
+            var commentUsersIds = [];
+            var usersData = [];
             console.log('$routeParams--------------------------------', $routeParams);
             if ($routeParams.threadId) {
                 SocialDataStore.getThreadByUniqueLink($routeParams.threadId).then(
@@ -15,6 +17,24 @@
                                     console.log('Success get Comments---------', data);
                                     if(data && data.data && data.data.result)
                                     Thread.comments=data.data.result;
+                                    Thread.comments.forEach(function(commentData) {
+                                        if(commentUsersIds.indexOf(commentData.userId) == -1) {
+                                            commentUsersIds.push(commentData.userId);
+                                        }
+                                    });
+                                    if(commentUsersIds && commentUsersIds.length > 0) {
+                                        SocialDataStore.getUsers(commentUsersIds).then(function (response) {
+                                            console.info('Users fetching for comments and response is: ', response.data.result);
+                                            if(response.data.error) {
+                                                console.error('Error while fetching users for comments ', response.data.error);
+                                            } else if(response.data.result) {
+                                                console.info('Users fetched successfully for comments ', response.data.result);
+                                                usersData = response.data.result;
+                                            }
+                                        }, function (err) {
+                                            console.log('Error while fetching users of comments inside thread page: ', err);
+                                        });
+                                    }
                                 },
                                 function (err) {
                                     console.log('Error get Comments----------', err);
@@ -39,5 +59,15 @@
                 );
             };
             console.log('Thread controller is loaded');
+            Thread.getUserName = function (userId) {
+                var userName = '';
+                usersData.some(function(userData) {
+                    if(userData.userObject._id == userId) {
+                        userName = userData.userObject.displayName || '';
+                        return true;
+                    }
+                });
+                return userName;
+            };
         }])
 })(window.angular);
